c9799784692e5fff24fb16d464dd6acea540e7aa
Lenovo <Lenovo@192.168.0.5>
2019-05-01 21:01:05 +0530

    Initial Commit

---
+++ API/src/main/java/WebServiceTesting/API/App.java
@@ -0,0 +1,13 @@
+package WebServiceTesting.API;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
